ef25712e9d4ebfbffe449d79ebc8707c8a28f0fe
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:54:44 +0000

    Create 07_Numbers.cs

---
+++ 01ChapterI_IntroductionInProgramming/07_Numbers.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace _07_Numbers
+{
+    class Chisla
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("1");
+            System.Console.WriteLine("101");
+            System.Console.WriteLine("1001");
+            Console.ReadKey();
+        }
+    }
+}
