edb54c23c755ff023450873ff6864b365014c091
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 21:43:41 +0000

    Create 06BoolMale.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/06BoolMale.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _06BoolMale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            Console.WriteLine(isMale);
+        }
+    }
+}
