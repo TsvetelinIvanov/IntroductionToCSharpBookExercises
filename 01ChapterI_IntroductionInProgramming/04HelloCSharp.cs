ee3f4c9492dd229bd3705556219ae152d37e5554
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:40:09 +0000

    Create 04HelloCSharp.cs

---
+++ 01ChapterI_IntroductionInProgramming/04HelloCSharp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _04HelloCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!");
+        }
+    }
+}
