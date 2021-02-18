b9c4b806075fd366af45367dd065072bae061fce
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-19 00:12:32 +0200

    Create 10Heart.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/10Heart.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace _10Heart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  OOO     OOO  ");
+            Console.WriteLine(" OOOOO   OOOOO ");
+            Console.WriteLine("OOOOOOO OOOOOOO");
+            Console.WriteLine("OOOOOOOOOOOOOOO");
+            Console.WriteLine("OOOOOOOOOOOOOOO");
+            Console.WriteLine(" OOOOOOOOOOOOO ");
+            Console.WriteLine("  OOOOOOOOOOO  ");
+            Console.WriteLine("   OOOOOOOOO   ");
+            Console.WriteLine("    OOOOOOO    ");
+            Console.WriteLine("     OOOOO     ");
+            Console.WriteLine("      OOO      ");
+            Console.WriteLine("       O       ");
+
+        }
+    }
+}
