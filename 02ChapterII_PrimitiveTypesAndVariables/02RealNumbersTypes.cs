927d22a63fec573898876f9d565b9330d222b0ab
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 21:24:42 +0000

    Create 02RealNumbersTypes.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/02RealNumbersTypes.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace _02RealNumbersTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a1 = 34.567839023;
+            float a2 = 12.345f;
+            float a3 = 8923.1234567f;
+            decimal a4 = 3456.091124875956542151256683467m;
+            Console.WriteLine(a1 + ", " + a2 + ", " + a3 + ", " + a4);
+        }
+    }
+}
