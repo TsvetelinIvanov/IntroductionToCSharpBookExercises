569c3d90f3ff57e94c7339a693d13ff3a4a1ee12
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 21:38:27 +0000

    Create 05IntToChar.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/05IntToChar.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _05IntToChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char H = (char)72;
+            Console.WriteLine(H); //H
+        }
+    }
+}
