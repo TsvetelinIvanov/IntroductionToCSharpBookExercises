53e2a93e1e3a059af49307a91444cbc244872d1c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 21:19:43 +0000

    Create 01WholeNumbersTypes.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/01WholeNumbersTypes.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01WholeNumbersTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ushort a1 = 52130;
+            sbyte a2 = -115;
+            int a3 = 4825932;
+            byte a4 = 97;
+            short a5 = -10000;
+            ushort a6 = 20000;
+            byte a7 = 224;
+            int a8 = 970700000;
+            byte a9 = 112;
+            sbyte a10 = -44;
+            int a11 = -1000000;
+            short a12 = 1990;
+            long a13 = 123456789123456789L;
+
+            Console.WriteLine(a1 + ", " + a2 + ", " + a3 + ", " + a4 + ", " + a5 + ", " + a6 + ", " + a7 + ", " + a8 + ", " + a9 + ", " + a10 + ", " + a11 + ", " + a12 + ", " + a13 + ".");
+        }
+    }
+}
