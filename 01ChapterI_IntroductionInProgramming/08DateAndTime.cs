ecfaefd71ae0ce5cce49a57f0f589db35335c082
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:56:42 +0000

    Create 08DateAndTime.cs

---
+++ 01ChapterI_IntroductionInProgramming/08DateAndTime.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace _08DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+        }
+    }
+}
