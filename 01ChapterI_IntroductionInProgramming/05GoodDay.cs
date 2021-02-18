c1bb46d0f3346a977d143528c95a50da4209f93a
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:45:32 +0000

    Create 05GoodDay.cs

---
+++ 01ChapterI_IntroductionInProgramming/05GoodDay.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _05GoodDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добър ден!");
+        }
+    }
+}
