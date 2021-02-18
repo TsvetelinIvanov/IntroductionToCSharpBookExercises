50abbbb458e978df6da315aca590cc54372a98da
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:50:32 +0000

    Create 06NameFamily.cs

---
+++ 01ChapterI_IntroductionInProgramming/06NameFamily.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _06NameFamily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tsvetelin Ivanov");
+        }
+    }
+}
