7ee5fb25c517f813250ce4ad056c5ade114d0257
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 20:59:32 +0000

    Create 09MathSqrt.cs

---
+++ 01ChapterI_IntroductionInProgramming/09MathSqrt.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _09MathSqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+    }
+}
