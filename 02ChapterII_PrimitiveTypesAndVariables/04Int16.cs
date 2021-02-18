7347b7b781dbc829d448a20897e4227faad9a420
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-18 21:30:59 +0000

    Create 04Int16.cs

---
+++ 02ChapterII_PrimitiveTypesAndVariables/04Int16.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _04Int16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 256;
+            int b = 0x100;
+            var c = "0x100(16) = 256(10)";
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
