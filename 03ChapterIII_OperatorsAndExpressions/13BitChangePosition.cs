da2606d0edd232777f84670b3bd0065072949c2c
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-19 06:08:14 +0200

    Create 13BitChangePosition.cs

---
+++ 03ChapterIII_OperatorsAndExpressions/13BitChangePosition.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace _13BitChangePosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int v = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+
+            if (v == 0)
+            {
+                v = n & (~(1 << p));//or v = ~(1 << p); v = v & n;
+                Console.WriteLine(v);
+            }
+            else if (v == 1)
+            {
+                v = n | (1 << p);//or v = 1 << p; v = v | n;
+                Console.WriteLine(v);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
+}
