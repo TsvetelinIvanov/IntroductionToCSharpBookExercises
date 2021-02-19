776d47b41ea2e6ee5ca6e05bb5df3c6d626ac679
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-19 06:12:14 +0200

    Create 14PrimeNumber.cs

---
+++ 03ChapterIII_OperatorsAndExpressions/14PrimeNumber.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace _14PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(n);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (n % divider == 0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? " + prime);
+        }
+    }
+}
