6437ec0aaef5ba1b26a65bc80264dfca9f78273e
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-20 09:43:56 +0200

    Create 07Max5Number.cs

---
+++ 05ChapterV_Conditions/07Max5Number.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace _07Max5Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            decimal nMax = decimal.MinValue;
+            for (int i = 0; i < n; i++)
+            {
+                decimal N = decimal.Parse(Console.ReadLine());
+                if (N > nMax)
+                {
+                    nMax = N;
+                }
+            }
+            Console.WriteLine("The biggest number is " + nMax + ".");
+        }
+    }
+}
