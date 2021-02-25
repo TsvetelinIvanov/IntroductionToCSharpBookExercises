bd1f83aa3e5ac1b7eed317eb8a30f3511a6b7c12
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 07:35:28 +0200

    Create 12DecimalToBinary.cs

---
+++ 06ChapterVI_Loops/12DecimalToBinary.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace _12DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Base = 2;
+            int number = int.Parse(Console.ReadLine());
+            
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            string numberToBaseString = string.Empty;
+            while (number > 0)
+            {
+                numberToBaseString = number % Base + numberToBaseString;
+                number /= Base;
+            }
+            
+            Console.WriteLine(numberToBaseString);
+        }
+    }
+}
