0f188fa0a4e7022177ee5eb288f69e9be128dff3
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 07:38:46 +0200

    Create 14DecimalToHexadecimal.cs

---
+++ 06ChapterVI_Loops/14DecimalToHexadecimal.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace _14DecimalToHexadecimal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int Base = 16;
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            string numberToBaseString = string.Empty;
+            while (number > 0)
+            {
+                int reminder = number % Base;
+                string reminderString = reminder.ToString();
+                if (reminder >= 10)
+                {
+                    switch (reminder)
+                    {
+                        case 10:
+                            reminderString = "A";
+                            break;
+                        case 11:
+                            reminderString = "B";
+                            break;
+                        case 12:
+                            reminderString = "C";
+                            break;
+                        case 13:
+                            reminderString = "D";
+                            break;
+                        case 14:
+                            reminderString = "E";
+                            break;
+                        case 15:
+                            reminderString = "F";
+                            break;                        
+                    }
+                }
+
+                numberToBaseString = reminderString + numberToBaseString;
+                number /= Base;
+            }
+            
+            Console.WriteLine(numberToBaseString);
+        }
+    }
+}
