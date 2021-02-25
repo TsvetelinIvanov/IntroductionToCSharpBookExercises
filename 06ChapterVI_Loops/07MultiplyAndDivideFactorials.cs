5a3a4da447e305323e113bfae7e807008b275bfd
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-02-25 07:24:31 +0200

    Create 07MultiplyAndDivideFactorials.cs

---
+++ 06ChapterVI_Loops/07MultiplyAndDivideFactorials.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Numerics;
+
+namespace _07MultiplyAndDivideFactorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int K = int.Parse(Console.ReadLine());
+            if (K >= N || K <= 1 || N <= 1)
+            {
+                Console.WriteLine("Invalid input!");
+                return;
+            }
+
+            BigInteger firstFactorial = 1;
+            for (int i = N; i > (N - K); i--)
+            {
+                firstFactorial *= i;
+            }
+
+            BigInteger secondFactorial = 1;
+            for (int i = K; i > 1; i--)
+            {
+                secondFactorial *= i;
+            }
+
+            BigInteger result = firstFactorial * secondFactorial;
+
+            Console.WriteLine(result);
+        }
+    }
+}
