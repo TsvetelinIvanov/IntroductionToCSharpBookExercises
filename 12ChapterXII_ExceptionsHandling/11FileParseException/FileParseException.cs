0eddc26d2995b037a329c6f6aa29d202b5fd8be1
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-13 18:45:46 +0200

    Add 11FileParseException

---
+++ 12ChapterXII_ExceptionsHandling/11FileParseException/FileParseException.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _11FileParseException
+{
+    public class FileParseException : Exception
+    {
+        private string fileName;
+        private string message;
+        private int row;
+
+        public FileParseException(string message)
+        {
+            this.message = message;
+        }
+
+        public FileParseException(string message, Exception innerException) : base(null, innerException)
+        {
+            this.message = message;
+        }
+
+        public FileParseException(string fileName, int row, Exception innerException)
+            : base(null, innerException)
+        {
+            this.FileName = fileName;
+            this.Row = row;
+        }
+
+        public FileParseException(string fileName, int row)
+        {
+            this.FileName = fileName;
+            this.Row = row;
+            this.message = string.Format("Invalid input format. Input: {0}. Row: {1}.", this.FileName, this.Row);
+        }
+
+        
+        public string FileName
+        {
+            get { return this.fileName; }
+            private set { this.fileName = value; }
+        }
+        
+        public int Row
+        {
+            get { return this.row; }
+            private set { this.row = value; }
+        }
+        
+        public override string Message
+        {
+            get { return this.message; }
+        }
+    }
+}
---
+++ 12ChapterXII_ExceptionsHandling/11FileParseException/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace _11FileParseException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            try
+            {
+                ParseLine(path);
+            }
+            catch (FileParseException fpe)
+            {
+                Console.WriteLine(fpe.Message);
+            }
+        }
+
+        static void ParseLine(string path)
+        {
+            StreamReader input;
+            try
+            {
+                input = new StreamReader(path);
+            }
+            catch (IOException e)
+            {
+                throw new FileParseException(path, e);
+            }
+
+            string line;            
+            int row = 0;
+            while ((line = input.ReadLine()) != null)
+            {
+                row++;                
+                try
+                {
+                    decimal number = decimal.Parse(line);
+                    Console.WriteLine(number);
+                }
+                catch (Exception)
+                {
+                    throw new FileParseException(path, row);
+                }
+            }
+        }
+    }
+}
