fa92da994fcd4a993f409d3c7ffab0cb8b729c36
TsvetelinIvanov <32267735+TsvetelinIvanov@users.noreply.github.com>
2021-03-13 18:48:36 +0200

    Add 13DownloadFile

---
+++ 12ChapterXII_ExceptionsHandling/13DownloadFile/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+
+namespace _13DownloadFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string address = Console.ReadLine();
+            string fileName = Console.ReadLine();
+            WebClient webClient = new WebClient();
+            try
+            {
+                webClient.DownloadFile(address, fileName);
+                Console.WriteLine("Donwload Complete.");
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine(ane.Message);
+            }            
+            catch (WebException we)
+            {
+                Console.WriteLine(we.Message);
+            }
+            catch (NotSupportedException nse)
+            {
+                Console.WriteLine(nse.Message);
+            }
+            finally
+            {
+                webClient.Dispose();
+            }
+        }
+    }
+}
